73fc0370a37b16e973304a37d40485fc9e4c8e0f
Divya Tiwari <me@divyatiwari.me>
2024-05-09 14:47:17 +0530

    Added navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,71 @@
+const NavBar = () => {
+  const dashboardItems = [
+    {
+      name: "Dashboard",
+      icon: "assets/dashboard.svg",
+      isNew: false,
+      isSeparateSection: false,
+    },
+    {
+      name: "Orders",
+      icon: "assets/order.svg",
+      isNew: false,
+      isSeparateSection: false,
+    },
+    {
+      name: "Customers",
+      icon: "assets/customer.svg",
+      isNew: false,
+      isSeparateSection: false,
+    },
+    {
+      name: "Products",
+      icon: "assets/product.svg",
+      isNew: false,
+      isSeparateSection: false,
+    },
+    {
+      name: "Pricing",
+      icon: "assets/pricing.svg",
+      isNew: true,
+      isSeparateSection: false,
+    },
+    {
+      name: "Freight",
+      icon: "assets/freight.svg",
+      isNew: true,
+      isSeparateSection: false,
+    },
+    {
+      name: "Settings",
+      icon: "assets/settings.svg",
+      isNew: false,
+      isSeparateSection: true,
+    },
+  ];
+
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <img src="assets/kenshiLogo.png" alt="logo" className="logo" />
+        <div className="item-container">
+          {dashboardItems.map((item, i) => (
+            <div key={i} className="item-section">
+              {item.isSeparateSection && <hr className="separator" />}
+              <div className="navbar-item">
+                <div className="item">
+                  <img src={item.icon} alt={item.name} />
+                  <span className="name">{item.name}</span>
+                </div>
+
+                {item.isNew && <span className="new-item">NEW</span>}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
